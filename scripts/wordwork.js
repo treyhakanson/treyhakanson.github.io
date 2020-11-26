71aedd9059de66a87cbda2d3fa2ce15270208fe5
treyhakanson <treyhakanson@gmail.com>
2020-11-25 21:50:17 -0500

    Add back mouse support

--- scripts/wordwork.js
+++ scripts/wordwork.js
@@ -5,55 +5,71 @@
   const clearButton = document.getElementById("clear");
   const recycleBin = document.getElementById("recycle");
 
-  root.addEventListener(
-    "touchstart",
-    function (e) {
-      if (e.target.classList.contains("letter--template")) {
-        e.preventDefault();
-        e.stopPropagation();
-        const { pageX, pageY } = e.changedTouches[0];
-        active = document.createElement("div");
-        active.classList.add("letter", "letter--placed");
-        active.textContent = e.target.textContent;
-        active.style.position = "absolute";
-        active.style.top = pageY;
-        active.style.left = pageX;
-        workspace.appendChild(active);
-      } else if (e.target.classList.contains("letter--placed")) {
-        e.preventDefault();
-        e.stopPropagation();
-        active = e.target;
-      }
-    },
-    false
-  );
-
-  root.addEventListener(
-    "touchmove",
-    function (e) {
-      if (active === null) {
-        return;
-      }
-
+  function getXAndY(e) {
+    if (e.changedTouches) {
       const { pageX, pageY } = e.changedTouches[0];
-      active.style.top = pageY;
-      active.style.left = pageX;
-    },
-    false
-  );
+      return { x: pageX, y: pageY };
+    } else {
+      return { y: e.pageY, x: e.pageX };
+    }
+  }
 
-  root.addEventListener(
-    "touchend",
-    function (e) {
-      const { pageX, pageY } = e.changedTouches[0];
-      const { x, y, width, height } = recycleBin.getBoundingClientRect();
-      if (pageX > x && pageX < x + width && pageY > y && pageY < y + height) {
-        active.remove();
-      }
-      active = null;
-    },
-    false
-  );
+  function dragStart(e) {
+    if (e.target.classList.contains("letter--template")) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      const { x, y } = getXAndY(e);
+
+      active = document.createElement("div");
+      active.classList.add("letter", "letter--placed");
+      active.textContent = e.target.textContent;
+      active.style.position = "absolute";
+      active.style.left = x;
+      active.style.top = y;
+
+      workspace.appendChild(active);
+    } else if (e.target.classList.contains("letter--placed")) {
+      e.preventDefault();
+      e.stopPropagation();
+
+      active = e.target;
+    }
+  }
+
+  function dragMove(e) {
+    console.log(e);
+    if (active === null) {
+      return;
+    }
+
+    const { x, y } = getXAndY(e);
+    active.style.left = x;
+    active.style.top = y;
+  }
+
+  function dragEnd(e) {
+    const { x: targetX, y: targetY } = getXAndY(e);
+    const { x, y, width, height } = recycleBin.getBoundingClientRect();
+    if (
+      targetX > x &&
+      targetX < x + width &&
+      targetY > y &&
+      targetY < y + height
+    ) {
+      active.remove();
+    }
+    active = null;
+  }
+
+  root.addEventListener("mousedown", dragStart, false);
+  root.addEventListener("touchstart", dragStart, false);
+
+  root.addEventListener("mousemove", dragMove, false);
+  root.addEventListener("touchmove", dragMove, false);
+
+  root.addEventListener("mouseup", dragEnd, false);
+  root.addEventListener("touchend", dragEnd, false);
 
   clearButton.addEventListener(
     "click",
