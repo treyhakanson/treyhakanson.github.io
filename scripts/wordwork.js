edb521ca8a49d207b51b1521180184383b24f342
treyhakanson <treyhakanson@gmail.com>
2020-11-25 21:13:59 -0500

    Update for touch devices

--- scripts/wordwork.js
+++ scripts/wordwork.js
@@ -4,33 +4,50 @@
   const workspace = document.getElementById("workspace");
   const clearButton = document.getElementById("clear");
 
-  root.addEventListener("mousedown", function (e) {
-    if (e.target.classList.contains("letter--template")) {
-      active = document.createElement("div");
-      active.classList.add("letter", "letter--placed");
-      active.textContent = e.target.textContent;
-      active.style.position = "absolute";
-      const { x, y, width, height } = e.target.getBoundingClientRect();
-      active.style.top = (height - y) / 2;
-      active.style.left = (width - x) / 2;
-      workspace.appendChild(active);
-    } else if (e.target.classList.contains("letter--placed")) {
-      active = e.target;
-    }
-  });
+  root.addEventListener(
+    "touchstart",
+    function (e) {
+      if (e.target.classList.contains("letter--template")) {
+        e.preventDefault();
+        e.stopPropagation();
+        const { pageX, pageY } = e.changedTouches[0];
+        active = document.createElement("div");
+        active.classList.add("letter", "letter--placed");
+        active.textContent = e.target.textContent;
+        active.style.position = "absolute";
+        active.style.top = pageY;
+        active.style.left = pageX;
+        workspace.appendChild(active);
+      } else if (e.target.classList.contains("letter--placed")) {
+        e.preventDefault();
+        e.stopPropagation();
+        active = e.target;
+      }
+    },
+    false
+  );
 
-  root.addEventListener("mousemove", function (e) {
-    if (active === null) {
-      return;
-    }
+  root.addEventListener(
+    "touchmove",
+    function (e) {
+      if (active === null) {
+        return;
+      }
 
-    active.style.top = e.clientY;
-    active.style.left = e.clientX;
-  });
+      const { pageX, pageY } = e.changedTouches[0];
+      active.style.top = pageY;
+      active.style.left = pageX;
+    },
+    false
+  );
 
-  root.addEventListener("mouseup", function (e) {
-    active = null;
-  });
+  root.addEventListener(
+    "touchend",
+    function (e) {
+      active = null;
+    },
+    false
+  );
 
   root.addEventListener("dblclick", function (e) {
     if (e.target.classList.contains("letter--placed")) {
@@ -38,9 +55,13 @@
     }
   });
 
-  clearButton.addEventListener("click", function (e) {
-    document
-      .querySelectorAll(".letter--placed")
-      .forEach((element) => element.remove());
-  });
+  clearButton.addEventListener(
+    "click",
+    function (e) {
+      document
+        .querySelectorAll(".letter--placed")
+        .forEach((element) => element.remove());
+    },
+    false
+  );
 })();
